1b03ff769f3544d2c3b10a7043396201e6376453
CavInc <cavinc20@gmail.com>
2017-05-16 10:56:05 +0700

    Добавлена обработка прав для A6+

--- app/src/main/java/ru/cav/medici/AddNewRecordActivity.java
+++ app/src/main/java/ru/cav/medici/AddNewRecordActivity.java
@@ -1,13 +1,16 @@
 package ru.cav.medici;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Vibrator;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -28,6 +31,7 @@ import ru.cav.medici.models.SpecChainModel;
 public class AddNewRecordActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 1000;
+    private static final String TAG = "BAR RECORD";
     private BarcodeDetector mBarcodeDetector;
     private CameraSource mCameraSource;
     private SurfaceView mCameraView;
@@ -42,41 +46,26 @@ public class AddNewRecordActivity extends AppCompatActivity {
 
         mCameraView = (SurfaceView) findViewById(R.id.surface_view);
 
-        mBarcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
+
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)){
                 ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CODE);
             }
+        } else {
+            createCamera();
         }
 
+        setTaskBar();
+    }
+
+    private void createCamera(){
+        mBarcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
         mCameraSource = new CameraSource.Builder(this, mBarcodeDetector)
                 .setAutoFocusEnabled(true).build();
 
-        mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-                try {
-                    mCameraSource.start(mCameraView.getHolder());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                mCameraSource.stop();
-            }
-        });
 
         mBarcodeDetector.setProcessor(new Detector.Processor() {
             @Override
@@ -93,18 +82,67 @@ public class AddNewRecordActivity extends AppCompatActivity {
                     Intent intent = new Intent(AddNewRecordActivity.this,ChangeActivity.class);
                     intent.putExtra(ConstantManager.CHANGE_FLG,ConstantManager.CHANGE_INSERT);
                     startActivityForResult(intent,ConstantManager.ADD_NEW_RECORD);
-                   // mCameraSource.stop();
+                    // mCameraSource.stop();
+
                 }
             }
         });
 
 
-        setTaskBar();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode!=PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+        if (grantResults.length!=0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            createCamera();
+            return;
+        }
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("PC")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCameraSource!=null){
+            mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @SuppressWarnings("MissingPermission")
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+                    try {
+                        mCameraSource.start(mCameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    mCameraSource.stop();
+                }
+            });
+        }
     }
 
     @Override
@@ -123,9 +161,10 @@ public class AddNewRecordActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                Log.d(TAG,"HOME");
                 onBackPressed();
                 return true;
         }
